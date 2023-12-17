24074fd38821aa8122b0ad8aa07731960034c081
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-17 22:52:43 +0300

    feat: add solution

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first > third && first > second?
+                result = first:
+                second > third ? second : third;
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        String result;
+        result = first > second ? "first" : "second";
+        System.out.println(result);
     }
 }
